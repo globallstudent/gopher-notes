526f3ac846c23573b0890a1dbeb4c43818d7b067
Asliddin Abdumannonov <asliddinabdumannonov06@gmail.com>
2025-05-28 02:17:27 +0500

    refactor testing hello-world

--- learning/learn-go-with-tests/01_hello_world/hello.go
+++ learning/learn-go-with-tests/01_hello_world/hello.go
@@ -2,17 +2,23 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 
 	if name == "" {
 		name = "world!"
+	}
+
+	if language == "Spanish" {
+		return spanishHelloPrefix + name
 	}
 
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("Chris"))
+	fmt.Println(Hello("Chris", ""))
 }

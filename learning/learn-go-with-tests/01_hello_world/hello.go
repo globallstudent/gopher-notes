cb171bc6b22e861d03e12b68bee20fbdc0453930
Asliddin Abdumannonov <asliddinabdumannonov06@gmail.com>
2025-05-28 00:35:02 +0500

    testing hello-world

---
+++ learning/learn-go-with-tests/01_hello_world/hello.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+const englishHelloPrefix = "Hello, "
+
+func Hello(name string) string {
+
+	if name == "" {
+		name = "world!"
+	}
+
+	return englishHelloPrefix + name
+}
+
+func main() {
+	fmt.Println(Hello("Chris"))
+}
